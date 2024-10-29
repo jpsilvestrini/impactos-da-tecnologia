5273a8c668d88b3a9ebbe8f59a96f158bc229ab8
subscr <joaopedrosilves18@gmail.com>
2024-10-29 00:47:19 -0300

    fix: wrong url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
 import "./globals.css";
 
 import type { Metadata, Viewport } from "next";
-import { Montserrat, Outfit } from "next/font/google";
+import { Outfit } from "next/font/google";
 
 import cn from "@/utils/cn";
 
 import { Provider } from "./provider";
-import { getBaseUrl } from "@/utils/urls";
 
 const outfit = Outfit({ subsets: ["latin"] });
-const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const viewport: Viewport = {
     themeColor: "#8957ff",
@@ -20,16 +18,12 @@ export const generateMetadata = async(): Promise<Metadata> => {
     const title = "Tecnologia na Educação: Transformando o Ensino Digital";
     const description = "Explore como a tecnologia está revolucionando a educação moderna. Descubra ferramentas interativas, plataformas de aprendizado adaptativo e recursos digitais que tornam o ensino mais envolvente e eficaz.";
     return {
-        metadataBase: new URL(getBaseUrl()),
         manifest: "/manifest.json",
         title: {
             default: title,
             template: "%s"
         },
         description,
-        alternates: {
-            canonical: getBaseUrl()
-        },
 
         creator: "João Pedro Silvestrini (subscr.is-a.dev)",
         publisher: "João Pedro Silvestrini (subscr.is-a.dev)",
